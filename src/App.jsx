dbf47992048c918f3f613a5cfba1b6ee4a706fcb
Lucas Costa <thelucascostadasilva@gmail.com>
2023-02-14 19:48:01 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import GlobalStyle from './styles/global';
+import RoutesApp from './routes';
+
+function App() {
+
+  return (
+    <>
+      <RoutesApp />
+      <GlobalStyle />
+    </>
+  )
+}
+
+export default App;
